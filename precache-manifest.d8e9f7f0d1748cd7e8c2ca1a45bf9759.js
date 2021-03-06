0041227a7aa8b09938bdf680bba2c6f1a95de4bb
reichert621 <reichertjalex@gmail.com>
2021-03-06 17:41:46 -0500

    Updates

---
+++ precache-manifest.d8e9f7f0d1748cd7e8c2ca1a45bf9759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c0e4c3d0629d1b2b7886506e44e2ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0079f2bb1dfd8b18545",
+    "url": "./static/css/main.ca9f16f5.chunk.css"
+  },
+  {
+    "revision": "edd852266149273a395f",
+    "url": "./static/js/2.be90e7b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.be90e7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0079f2bb1dfd8b18545",
+    "url": "./static/js/main.eb130053.chunk.js"
+  },
+  {
+    "revision": "298dc666b7c78f2a119d",
+    "url": "./static/js/runtime-main.983f3cd1.js"
+  }
+]);
