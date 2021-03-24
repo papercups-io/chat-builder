653821e3be121e67a406fce0e7ab78eccb980f4d
Robert Honsby <rhonsby@gmail.com>
2021-03-24 15:03:19 -0400

    Adds exporting of types

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,44 @@
-import ChatBuilder from './components/ChatBuilder';
+import ChatBuilder, {
+  BodyProps,
+  Config,
+  FooterProps,
+  HeaderProps,
+  NotificationsProps,
+  OnSendMessage,
+  Options,
+  ScrollToRef,
+  State,
+  ToggleButtonProps,
+} from './components/ChatBuilder';
 import ChatHeader from './components/ChatHeader';
 import ChatBody from './components/ChatBody';
 import ChatFooter from './components/ChatFooter';
 import ChatToggle from './components/ChatToggle';
+import {
+  User,
+  Message,
+  Attachment,
+  CustomerMetadata,
+  WidgetSettings,
+  WidgetConfig,
+} from './helpers/types';
 
+export type {
+  Attachment,
+  BodyProps,
+  Config,
+  CustomerMetadata,
+  FooterProps,
+  HeaderProps,
+  Message,
+  NotificationsProps,
+  OnSendMessage,
+  Options,
+  ScrollToRef,
+  State,
+  ToggleButtonProps,
+  User,
+  WidgetConfig,
+  WidgetSettings,
+};
 export {ChatBuilder, ChatHeader, ChatBody, ChatFooter, ChatToggle};
