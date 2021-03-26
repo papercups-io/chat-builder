7428c5a59a1ec3255fa39895a20e175ce831d887
reichert621 <reichertjalex@gmail.com>
2021-03-25 20:18:26 -0400

    Test adding nes example to gh pages

---
+++ nes/precache-manifest.da57ada3ee4bf86fc5fd296ef6289318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33d6f25a0d299891783e4220c53a408",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc4f77be1962b8238ab5",
+    "url": "./static/css/2.4d48fd3d.chunk.css"
+  },
+  {
+    "revision": "7a2c3d04f317bdd274e0",
+    "url": "./static/css/main.0322cc66.chunk.css"
+  },
+  {
+    "revision": "cc4f77be1962b8238ab5",
+    "url": "./static/js/2.bb56ba86.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.bb56ba86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2c3d04f317bdd274e0",
+    "url": "./static/js/main.50a87580.chunk.js"
+  },
+  {
+    "revision": "94889b7ee81db3269955",
+    "url": "./static/js/runtime-main.4b8bda32.js"
+  }
+]);
