9acafee1a7e95cd50ad9f79aaf10e1f0cf5313e4
reichert621 <reichertjalex@gmail.com>
2021-04-12 21:40:09 -0400

    Updates

---
+++ supabase/build/precache-manifest.a2d4cce53c6470c92baca64724e57376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31dca4d2cf3f3b0142a71c8409058024",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a28d1cf6329f7a1e158a",
+    "url": "./static/css/main.a25e7281.chunk.css"
+  },
+  {
+    "revision": "829e9a4e5586a0798432",
+    "url": "./static/js/2.85235770.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "./static/js/2.85235770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd94eacf7e9c3a0201eb",
+    "url": "./static/js/3.a51fce0c.chunk.js"
+  },
+  {
+    "revision": "a28d1cf6329f7a1e158a",
+    "url": "./static/js/main.73be4c2b.chunk.js"
+  },
+  {
+    "revision": "69e3f624847470d38916",
+    "url": "./static/js/runtime-main.0499b3de.js"
+  }
+]);
