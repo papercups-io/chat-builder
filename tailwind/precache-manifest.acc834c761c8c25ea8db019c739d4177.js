2d16346609d94e1803bf423ed5720466dba19d04
reichert621 <reichertjalex@gmail.com>
2021-04-13 13:20:43 -0400

    Updates

---
+++ tailwind/precache-manifest.acc834c761c8c25ea8db019c739d4177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61ad6134bf10f71ead9f278e2e162b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc1d94719cf1672f6719",
+    "url": "./static/css/main.fd945348.chunk.css"
+  },
+  {
+    "revision": "f07b7eadf900ef93ea07",
+    "url": "./static/js/2.ad124949.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.ad124949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd25323d6d31e6de204",
+    "url": "./static/js/3.c45112af.chunk.js"
+  },
+  {
+    "revision": "cc1d94719cf1672f6719",
+    "url": "./static/js/main.5162ffda.chunk.js"
+  },
+  {
+    "revision": "da2324f378c4fc1afb6c",
+    "url": "./static/js/runtime-main.c2ed4e2d.js"
+  }
+]);
